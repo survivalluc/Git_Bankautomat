a86ae556a5756b4f41af9c303df201f79186e476
Offline <Offline@192.168.1.78>
2016-11-07 22:45:47 +0100

    First Commit

---
+++ DerGerät.java
@@ -0,0 +1,18 @@
+
+public class DerGerät {
+
+	/**
+	 * Test :D
+	 * Lucas Giertz 17.09.2015
+	 * Copyright by Lucas Giertz
+	 */
+	public static void main(String[] args) {
+		Integer lucas=44;
+		System.out.println(lucas + " ist eine Zahl :D");
+		System.out.println("400000000000000");
+		
+	
+
+	}
+
+}
